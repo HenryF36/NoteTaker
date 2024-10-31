6f716687d1f567b116819b023eb482f566fb53d4
Henry F <HenryFabinski36@outlook.com>
2024-10-31 17:12:34 -0400

    Added New Note Window

--- app/src/main/java/com/hfcode/notetaker/MainActivity.kt
+++ app/src/main/java/com/hfcode/notetaker/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.hfcode.notetaker
 
+import android.app.Dialog
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.EditText
 import androidx.activity.ComponentActivity
 import com.hfcode.notetaker.R
 
@@ -14,11 +17,31 @@ class MainActivity : ComponentActivity() {
         val openNoteButton: Button = findViewById(R.id.OpenNoteB)
         openNoteButton.setOnClickListener {
             Log.d("NoteTakerL", "Open Note Start")
+
         }
 
         val newNoteButton: Button = findViewById(R.id.NewNoteB)
         newNoteButton.setOnClickListener {
             Log.d("NoteTakerL", "New Note Start")
+            // Initialize dialog
+            val dialog = Dialog(this)
+            dialog.setContentView(R.layout.activity_new_note)
+
+            // Set dialog properties
+            dialog.setCancelable(true)
+
+            // Customize dialog elements
+            //val closeButton = dialog.findViewById<Button>(R.id.CloseB)
+            //val NoteNameIm = dialog.findViewById<EditText>(R.id.EnterPassword)
+
+            //closeButton.setOnClickListener {
+                // Close Button
+
+            //}
+
+            // Show the dialog
+            dialog.show()
+            Log.d("NoteTakerL","NewNoteAct started.")
         }
 
         val viewAllNotesButton: Button = findViewById(R.id.ViewAllN)
